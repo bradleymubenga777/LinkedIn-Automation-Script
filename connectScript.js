d23aa89d4020ff52e43e67c39ad4ce5d911b50fe
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-01-10 21:11:38 +0200

    Update connectScript.js

--- connectScript.js
+++ connectScript.js
@@ -1,35 +1,21 @@
 //Storing components and values in variables.
-const connectButtons = document.querySelectorAll('.buttonClassNames');
-const inviteMessage = document.getElementById("inputId");
-const inviteButtons = document.querySelectorAll('.buttonClassNames');
-var nameOfTheProspect = document.getElementById("idOfNameNode").textContent;
+let connectButton = document.querySelectorAll('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
+let addANoteBtn = document.querySelectorAll('.mr1.artdeco-button.artdeco-button--muted.artdeco-button--3.artdeco-button--secondary.ember-view');
+let inviteMessage = document.getElementById("custom-message");
+let sendInviteButton = document.querySelectorAll('.buttonClassNames');
+var nameOfTheProspect = document.getElementsByTagName('strong');
+var nameOfClient = nameOfTheProspect[0];
 
 //A loop that will run as long as users are less than and equal to 50.
-for (nthUser = 50;  connectButtons.length <= nthUser;)
+for (i = 0;  connectButton.length <= i; i++)
 {
-    //Click The Conncection Button.
-    connectButtons.click()
-    .then((success) => {
-        //Wait till button clicks then set the input value value to the string.
-        inviteMessage.value = `Invite Message Text With ${nameOfTheProspect}`;
-        success = "Input Value Was Set Successfuly"
-        console.log(success);
-    })
-    .catch((err) => {
-        err = "ERROR 111: could not send set the value"
-        console.log(err);
-    })
+    userBtn = connectButtons[i];
 
-    //Check if input value was set successfuly then press the invite button.
-    if (inviteMessage.length)
+    if (userBtn.textContent.length == 14)
     {
-        inviteButtons.click();
-        console.log("Invites sent successfuly!");
+        userBtn.click();
     }
 
-    else 
-    {
-        console.log("ERROR 222: could not send invites.");
-    }
+    inviteMessage.value = "Text String!"
 
 };
