89f42c3a66cf031b2fa5624157f663b655f28b6b
Bradley Mubenga <bradleymubenga777@gmail.com>
2021-01-16 16:23:08 +0200

    Update: connectScript.js

--- connectScript.js
+++ connectScript.js
@@ -1,21 +1,26 @@
-//Storing components and values in variables.
-let connectButton = document.querySelectorAll('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
-let addANoteBtn = document.querySelectorAll('.mr1.artdeco-button.artdeco-button--muted.artdeco-button--3.artdeco-button--secondary.ember-view');
-let inviteMessage = document.getElementById("custom-message");
-let sendInviteButton = document.querySelectorAll('.buttonClassNames');
-var nameOfTheProspect = document.getElementsByTagName('strong');
-var nameOfClient = nameOfTheProspect[0];
+///// Declaring Variables And Functions
+let buttons = document.querySelectorAll('.artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view');
+let connectName;
+
+/////
+function clickingAddNote() {
+    let addANoteBtn = document.querySelectorAll('.mr1.artdeco-button.artdeco-button--muted.artdeco-button--3.artdeco-button--secondary.ember-view');
+    let clickAddNote = addANoteBtn[0];
+    var accessName = document.getElementsByClassName('flex-1');
+    
+    setTimeout(() => {
+        connectName = accessName[0].childNodes[2].textContent;
+        clickAddNote.click();
+    }, 2000)
+}
+
+function addMessage() {
+    setTimeout(() => {
+        let inviteMessage = document.getElementById("custom-message");
+        inviteMessage.value = `Hello ${connectName}`;
+    }, 2000);
+}
 
 
-for (i = 0;  connectButton.length <= i; i++)
-{
-    userBtn = connectButtons[i];
-
-    if (userBtn.textContent.length == 14)
-    {
-        userBtn.click();
-    }
-
-    inviteMessage.value = "Text String!"
-
-};
+//// One Line Script
+buttons.forEach(button => {if (button.innerText.length == 7) {button.click(), clickingAddNote(), addMessage()} });
